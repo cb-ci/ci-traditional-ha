01d0acac21663626968a44c34b05656cc692b90a
cccaternberg <acaternberg@cloudbees.com>
2025-09-08 11:16:23 +0200

    add

--- env.sh
+++ env.sh
@@ -2,6 +2,8 @@
 
 set +x
 
+export logLine="ci-traditional-ha"
+
 echo "#### SSH Key settings"
 # An SSH key gets generated under this path when you run up.sh
 export SSH_KEY_ID="agent_id_rsa"
@@ -46,6 +48,8 @@ export CLIENT2_IP=$IP_PREFIX.0.8
 export CLIENT3_IP=$IP_PREFIX.0.11
 export AGENT_IP=$IP_PREFIX.0.9
 export BROWSER_IP=$IP_PREFIX.0.10
+export OTEL_IP=$IP_PREFIX.0.12
+export JAEGER_IP=$IP_PREFIX.0.2
 
 ########################################################################################################################
 
@@ -72,7 +76,9 @@ export SPLUNK_PERSISTENCE=$PERSISTENCE_PREFIX/splunk-data
 export BROWSER_PERSISTENCE=$PERSISTENCE_PREFIX/browser
 export HAPROXY_PERSISTENCE=$PERSISTENCE_PREFIX/haproxy
 export OC_PERSISTENCE=$PERSISTENCE_PREFIX/oc
+export OC_TMP=$PERSISTENCE_PREFIX/oc-tmp
 export CONTROLLER_PERSISTENCE=$PERSISTENCE_PREFIX/controllers
+export CONTROLLER_TMP=$PERSISTENCE_PREFIX/controllers-tmp
 export CONTROLLER1_CACHES=$PERSISTENCE_PREFIX/controller1_caches
 export CONTROLLER2_CACHES=$PERSISTENCE_PREFIX/controller2_caches
 export CONTROLLER3_CACHES=$PERSISTENCE_PREFIX/controller3_caches
@@ -87,6 +93,12 @@ echo "#### Controller settings"
 export CONTROLLER_JENKINS_OPTS="--webroot=/var/cache/cloudbees-core-cm/war --pluginroot=/var/cache/cloudbees-core-cm/plugins"
 #https://docs.cloudbees.com/docs/cloudbees-ci/latest/ha/specific-ha-installation-traditional#_java_options
 export CONTROLLER_JAVA_OPTS="--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED --add-modules=java.se --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.management/sun.management=ALL-UNNAMED --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED -Djenkins.model.Jenkins.crumbIssuerProxyCompatibility=true -DexecutableWar.jetty.disableCustomSessionIdCookieName=true -Dcom.cloudbees.jenkins.ha=false -Dcom.cloudbees.jenkins.replication.warhead.ReplicationServletListener.enabled=true -Djenkins.plugins.git.AbstractGitSCMSource.cacheRootDir=/var/cache/cloudbees-core-cm/caches/git -Dorg.jenkinsci.plugins.github_branch_source.GitHubSCMSource.cacheRootDir=/var/cache/cloudbees-core-cm/caches/github-branch-source -XX:+AlwaysPreTouch -XX:+UseStringDeduplication -XX:+ParallelRefProcEnabled -XX:+DisableExplicitGC"
+
+#Set /tmp dir to mounted volume to avoid "No space left on device".
+#Jenkins (Jetty) unpacks the WAR into ${java.io.tmpdir} = /tmp. In Docker Desktop on macOS, /tmp lives inside the Linux VM/disk-image. If that VM’s disk (or inodes) is full, you’ll get: No space left on device
+#So we point /tmp to unpack the war to a another dircetory . See also controller mounted volumes in docker-compose
+export CONTROLLER_JAVA_OPTS="$CONTROLLER_JAVA_OPTS -Djava.io.tmpdir=/var/cache/cloudbees-core-cm/tmp"
+
 # https://docs.cloudbees.com/docs/cloudbees-ci/latest/casc-oc/configure-oc-traditional#_adding_the_java_system_property
 # We assign the controller casc bundle directly, comment out if you don't want to use casc
 #export CONTROLLER_JAVA_OPTS="$CONTROLLER_JAVA_OPTS -Dcore.casc.config.bundle=/var/jenkins_home/bundle-link.yaml"
@@ -97,6 +109,11 @@ export CONTROLLER_JAVA_OPTS="$CONTROLLER_JAVA_OPTS -Dcore.casc.config.bundle=/va
 
 echo "#### Operations center setting"
 export CJOC_JAVA_OPTS="-XX:+AlwaysPreTouch -XX:+UseStringDeduplication -XX:+ParallelRefProcEnabled -XX:+DisableExplicitGC -Dcom.cloudbees.jenkins.ha=false"
+
+#Set /tmp dir to mounted volume to avoid "No space left on device".
+# Jenkins (Jetty) unpacks the WAR into ${java.io.tmpdir} = /tmp. In Docker Desktop on macOS, /tmp lives inside the Linux VM/disk-image. If that VM’s disk (or inodes) is full, you’ll get:
+export CJOC_JAVA_OPTS="$CJOC_JAVA_OPTS -Djava.io.tmpdir=/var/jenkins_home/tmp"
+
 # https://docs.cloudbees.com/docs/cloudbees-ci/latest/casc-oc/configure-oc-traditional#_adding_the_java_system_property
 # We assign the cjoc casc bundle, comment out if you don't want to use casc
 export CJOC_JAVA_OPTS="$CJOC_JAVA_OPTS -Dcore.casc.config.bundle=/var/jenkins_home/cascbundle/cjoc"
